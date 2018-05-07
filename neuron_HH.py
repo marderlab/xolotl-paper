eb6028ac238b8c5acb94a8fe3651bd40a1afdb9d
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-07 16:38:18 -0400

    Do speed tests

--- neuron_HH.py
+++ neuron_HH.py
@@ -1,14 +1,13 @@
 from neuron import h, gui
 import time
 
-print(time.clock())
 # create sections
 soma = h.Section(name = 'soma')
 h.psection(sec=soma)
 
 # set the size of the soma
-soma.L = 400
-soma.diam = 50
+soma.L = 50
+soma.diam = 25
 print('surface area of the soma = {}'.format(soma(0.5).area()))
 
 # split into 101 compartments
@@ -17,22 +16,31 @@ h.psection(soma)
 
 # add Hodgkin-Huxley conductances
 soma.insert('hh')
+# inject current
+stim = h.IClamp(soma(0.5))
+stim.amp = 1.0;
+stim.dur = 5000;
 
 # set up output vectors
 v_vec = h.Vector()        # Membrane potential vector
 t_vec = h.Vector()        # Time stamp vector
 v_vec.record(soma(0.5)._ref_v)
 t_vec.record(h._ref_t)
-h.tstop = 1000.0 # s
+
+# set up simulation
+h.dt    = 0.05/1000 # s
+h.tstop = 100.0 # s
+h.secondorder = 2 # use Crank-Nicholson
 
 # run simulation
+aaa = time.clock()
 h.run()
-print(time.clock())
+print(time.clock()-aaa)
 
 # visualize
 from matplotlib import pyplot
 pyplot.figure(figsize=(8,4)) # Default figsize is (8,6)
 pyplot.plot(t_vec, v_vec)
-pyplot.xlabel('time (ms)')
+pyplot.xlabel('time (s)')
 pyplot.ylabel('mV')
 pyplot.show()
--- xolotl_comps.m
+++ xolotl_comps.m
@@ -1,21 +1,40 @@
 % make figure of slice time over number of slices
-nSlices = 2:100;
-time    = zeros(length(nSlices),1);
+nSlices = 2:10:100;
+time    = zeros(length(nSlices),2);
+I_ext   = 100 * ones(max(nSlices),1);
 
-for ii = nSlices
+for ii = 1:length(nSlices)
   textbar(ii,length(nSlices))
-  x = make_neuron();
-  x.AB.radius = 25;
-  x.AB.len = 400;
-  x.sha1hash;
+  % create xolotl object with two conductances
+  x = xolotl;
+  x.add('HH','compartment','V', -65, 'Ca', 0.02, 'Cm', 10, 'A', 0.0628, ...
+    'radius', 25, 'len', 400);
+  x.HH.add('liu-approx/NaV', 'gbar', 1000, 'E', 50);
+  x.HH.add('liu-approx/Kd', 'gbar', 300, 'E', -80);
+  x.HH.add('Leak', 'gbar', 1, 'E', -50);
+  % test slicing speed
   tic;
-  x.slice('AB',nSlices(ii),100);
-  time(ii) = toc;
+  x.slice('HH',nSlices(ii),100);
+  time(ii,1) = toc;
+  % test simulation speed
+  tic;
+  x.integrate(I_ext(1:nSlices(ii)));
+  time(ii,2) = toc;
 end
 
 figure;
-plot(nSlices,time)
+
+subplot(1,2,1)
+plot(nSlices,time(:,1))
 xlabel('# of slices')
 ylabel('time (s)')
-title('runtime vs. slicing')
-prettyFig();
+title('compile-time vs. slicing')
+
+subplot(1,2,2)
+hold on
+plot(nSlices,time(:,2))
+xlabel('# of slices')
+ylabel('time (s)')
+title('run-time vs. slicing')
+
+equalizeAxes(); prettyFig();
