d7fe6a14574781d1b0a290020423b6adeaffd0d8
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-08 13:27:14 -0400

    Test speed against 10-100 compartments

--- xolotl_comps.m
+++ xolotl_comps.m
@@ -1,24 +1,26 @@
 % make figure of slice time over number of slices
-nSlices = 2:10:100;
-time    = zeros(length(nSlices),2);
-I_ext   = 100 * ones(max(nSlices),1);
+nSlices = linspace(10,100,10);
+time    = zeros(length(nSlices),1);     % s
+I_ext   = 0.1 * ones(max(nSlices),1);   % nA
 
 for ii = 1:length(nSlices)
   textbar(ii,length(nSlices))
   % create xolotl object with two conductances
   x = xolotl;
-  x.add('HH','compartment','V', -65, 'Ca', 0.02, 'Cm', 10, 'A', 0.0628, ...
-    'radius', 25, 'len', 400);
+  x.add('HH','compartment','V', -65, 'Ca', 0.02, 'Cm', 10, ...
+    'radius', 0.025, 'len', 0.050);
   x.HH.add('liu-approx/NaV', 'gbar', 1000, 'E', 50);
   x.HH.add('liu-approx/Kd', 'gbar', 300, 'E', -80);
   x.HH.add('Leak', 'gbar', 1, 'E', -50);
-  % test slicing speed
-  tic;
   x.slice('HH',nSlices(ii),100);
-  time(ii,1) = toc;
+  x.sha1hash;
+  % set up simulation
+  x.t_end = 100e3;  % ms
+  x.dt = 0.05;      % ms
+  V = zeros(x.t_end/x.dt,nSlices(ii));
   % test simulation speed
   tic;
-  x.integrate(I_ext(1:nSlices(ii)));
+  V = x.integrate(I_ext(1:nSlices(ii)));
   time(ii,2) = toc;
 end
 
@@ -28,13 +30,13 @@ subplot(1,2,1)
 plot(nSlices,time(:,1))
 xlabel('# of slices')
 ylabel('time (s)')
-title('compile-time vs. slicing')
+title('run-time vs. slicing')
 
 subplot(1,2,2)
 hold on
-plot(nSlices,time(:,2))
+plot(nSlices,x.t_end/1000/time(:,2))
 xlabel('# of slices')
-ylabel('time (s)')
-title('run-time vs. slicing')
+ylabel('speed factor')
+title('sim-time/run-time vs. slicing')
 
 equalizeAxes(); prettyFig();
