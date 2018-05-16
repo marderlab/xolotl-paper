5d9c1ea2c6a932b7a941290b16459a04c162b7d0
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-16 18:25:39 -0400

    Rework figure to incorporate full voltage clamp procedure

--- figures/figure_clamp.m
+++ figures/figure_clamp.m
@@ -31,16 +31,16 @@ ax(2) = subplot(3,4,5);
 % code snipped
 ax(3) = subplot(3,4,9);
 % current vs. time for stepped voltage
-ax(4) = subplot(2,4,2);
+ax(4) = subplot(2,4,2); hold on;
 % voltage vs. time for stepped voltage
-ax(5) = subplot(2,4,6);
+ax(5) = subplot(2,4,6); hold on;
 % current vs. voltage
 ax(6) = subplot(2,4,3);
 % conductance vs. voltage
 ax(7) = subplot(2,4,7);
-% R^2 fit
-ax(8) = subplot(2,4,4);
 % steady-state vs. voltage
+ax(8) = subplot(2,4,4);
+% R^2 fit
 ax(9) = subplot(2,4,8);
 
 %% Make Cartoon Cell
@@ -66,58 +66,99 @@ ax(1).Tag = 'xolotl_printout';
 x.closed_loop = true;
 Vhold				= -60;
 Vsteps 			= linspace(-80, 50, 30);
-current 		= NaN(2 * x.t_end/x.dt, length(Vsteps));
+current 		= NaN(x.t_end/x.dt, length(Vsteps));
+voltage 		= NaN(x.t_end/x.dt, length(Vsteps));
+time 				= x.dt * (1:length(current));
 
+% compute current vs. time over voltage steps
 for ii = 1:length(Vsteps)
 	% let the current reach steady-state
+	x.t_end = 5e2;
 	x.integrate([], Vhold);
 	% save the current at this initial state
-	c = x.integrate([], Vhold);
+	x.t_end = 1e2;
+	c1 = x.integrate([], Vhold);
+	v1 = Vhold * ones(length(c1), 1);
 	% perform the voltage step
-	ctrace 		= [c; x.integrate([], Vsteps(ii))];
-	current(:, ii) = ctrace(:);
+	x.t_end = 4e2;
+	c2 = x.integrate([], Vsteps(ii));
+	v2 = Vsteps(ii) * ones(length(c2), 1);
 	% clean up simulation artifact
-	current(x.t_end/x.dt + 1, ii) = current(x.t_end/x.dt, ii);
+	% c2(1) = c1(end);
+	ctrace = [c1; c2];
+	% store the current
+	current(:, ii) = ctrace(:);
+	% store the voltage
+	voltage(:, ii) = [v1; v2];
 end
 
-return
+%% Plot Current vs. Time over Voltage Steps
 
-% perform the voltage steps
-
-for i = 1:length(all_V_step)
-	all_I(:,i) = x.integrate([],all_V_step(i));
+c = parula(floor(1.1*length(Vsteps)));
+for ii = 1:length(Vsteps)
+	plot(ax(4), time, current(:, ii), 'Color', c(ii, :))
 end
 
-time = (1:length(all_I))*x.dt;
-
-%% Plot Voltage vs. Time
-
-c = parula(length(all_V_step));
-for ii = 1:length(all_V_step)
-	Vstep = all_V_step(ii)*ones(length(time),1);
-	plot(ax(4), time, Vstep, 'Color', c(ii, :));
-end
 xlabel(ax(4), 'time (ms)')
-ylabel(ax(4), 'voltage clamp (mV)')
+ylabel(ax(4), 'current (nA)')
+set(ax(4), 'XLim', [90 120], 'YLim', [1.5*min(vectorise(current)) 1.1*max(vectorise(current))])
 
-%% Plot Current vs. Time
+%% Plot Voltage vs. Time over Voltage Steps
 
-for i = 1:length(all_V_step)
-	plot(ax(5), time, all_I(:,i), 'Color', c(i,:))
+for ii = 1:length(Vsteps)
+	plot(ax(5), time, voltage(:, ii), 'Color', c(ii, :));
 end
+
 xlabel(ax(5), 'time (ms)')
-ylabel(ax(5), 'current (nA)')
-set(ax(5), 'XScale', 'log', 'XLim', [1e-2 1e1], 'XTick', [1e-2 1e-1 1e0 1e1])
+ylabel(ax(5), 'voltage (mV)')
+set(ax(5), 'XLim', [90 120], 'YLim', [-90 60], 'YTick', [-80 -40 0 40]);
 
 %% Plot Current vs. Voltage
 
-plot(ax(6), all_V_step, all_I(end,:), 'r')
+plot(ax(6), Vsteps, current(end,:), 'k')
 xlabel(ax(6), 'voltage clamp (mV)')
 ylabel(ax(6), 'current (nA)')
+set(ax(6), 'XLim', [min(Vsteps) max(Vsteps)], 'XTick', [-80 -40 0 40])
+
+%% Plot Conductance vs. Voltage
+conductance = current(end, :) ./ (Vsteps - x.AB.Kd.E) / x.AB.A;
+plot(ax(7), Vsteps, conductance, 'k');
+xlabel(ax(7), 'voltage clamp (mV)')
+ylabel(ax(7), 'ḡ (μS/mm^2)')
+set(ax(7), 'XLim', [min(Vsteps) max(Vsteps)], 'XTick', [-80 -40 0 40])
+
+%% Plot Steady-State Kd Activation Gating Variable
+
+minf_func = x.getGatingFunctions('Kd');
+for ii = 1:length(Vsteps)
+	minf(ii) = minf_func(Vsteps(ii));
+end
+plot(ax(8), Vsteps, minf.^4, 'k')
+xlabel(ax(8), 'voltage clamp (mV)')
+set(ax(8), 'XLim', [min(Vsteps) max(Vsteps)], 'XTick', [-80 -40 0 40])
+
+%% Plot R^2 value
+
+all_n 		= 1:4;
+all_r2 		= Inf*all_n;
+warning off
+for j = 1:4
+	temp = conductance.^(1/j);
+	rm_this = isnan(temp) | isinf(temp);
+	all_r2(j) = rsquare(temp(~rm_this),minf(~rm_this));
+end
+warning on
+
+[maxr2,idx] = max(all_r2);
+fprintf(['[Best fit with n = ' oval(idx) ', r2 = ' oval(maxr2) ']']);
+
+plot(ax(9), all_n, 1-all_r2, 'k')
+xlabel(ax(9), 'exponent')
+ylabel(ax(9), 'R^2 error')
 
 %% Post-Processing
 
-prettyFig()
+prettyFig('fs', 12, 'lw', 1)
 
 for ii = 1:length(ax)
   box(ax(ii), 'off')
