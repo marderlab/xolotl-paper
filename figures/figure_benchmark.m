9302f10dda70413521053caa67f14808ac3bcd80
Alec Hoyland <ahoyland@brandeis.edu>
2018-05-24 21:31:29 -0400

    Fix issues with error assessment, finish making figure

--- figures/figure_benchmark.m
+++ figures/figure_benchmark.m
@@ -20,7 +20,7 @@ x.HH.add('liu/NaV', 'gbar', 1000, 'E', 50);
 x.HH.add('liu/Kd', 'gbar', 300, 'E', -80);
 x.HH.add('Leak', 'gbar', 1, 'E', -50);
 
-t_end = 0.5e3;
+t_end = 5e3;
 x.t_end = t_end;
 
 % set up DynaSim equation block
@@ -54,10 +54,9 @@ sim_time          = NaN(length(dt), 3); % duration of runtime
 r2                = NaN(length(dt), 3); % correlation coefficient
 
 % perform benchmark for xolotl
-x.sim_dt          = dt(1);
-x.dt              = max_dt / 1e3;
 
-% set up "best-case"  for accuracy
+x.closed_loop = false;
+
 x.sim_dt = dt(1);
 x.dt = max_dt*1e-3;
 tic
@@ -65,9 +64,9 @@ V0 = x.integrate(0.2);
 V0_diff = diff(V0);
 sim_time(1,1) = toc;
 
-% run benchmark test
-for i = 2:length(dt)
 
+for i = 2:length(dt)
+  textbar(i, length(dt))
 	x.sim_dt = dt(i);
 	tic
 	V = x.integrate(0.2);
@@ -86,18 +85,18 @@ for i = 2:length(dt)
 	r2(i,1) = this_cost;
 
 end
-return
+
 % perform benchmark for DynaSim
 sim_dt            = dt(1);
 sampling          = max_dt / 1e3;
 
 % precompile
-data              = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end], 'dt', sim_dt, 'compile_flag', 1);
+data              = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end], 'dt', sim_dt, 'compile_flag', 0);
 
 % set up "best-case" for accuracy
 tic
-data0             = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end], 'dt', dt(1), 'compile_flag', 1, 'downsample_factor', 1/dt(1));
-sim_time(1,:)     = toc;
+data0             = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end], 'dt', dt(1), 'compile_flag', 0, 'downsample_factor', 1/dt(1));
+sim_time(1,2)     = toc;
 V0                = data0.(data0.labels{1});
 V0_diff           = diff(V0);
 
@@ -105,7 +104,7 @@ for ii = 2:length(dt)
   textbar(ii, length(dt))
   sim_dt          = dt(ii);
   tic
-  data            = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end], 'dt', sim_dt, 'compile_flag', 1, 'downsample_factor', 1/sim_dt);
+  data            = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end], 'dt', sim_dt, 'compile_flag', 0, 'downsample_factor', 1/sim_dt);
   sim_time(ii, 2) = toc;
 
   % measure distance between diff-embedded attractors
@@ -121,7 +120,7 @@ for ii = 2:length(dt)
   r2(ii, 2)       = this_cost;
 end
 
-%% Plot Benchmark Test #1
+%% Plot Benchmark Test #1 & #2
 
 c = lines(100);
 
@@ -129,7 +128,7 @@ c = lines(100);
 Qfactor = t_end / 1e3 ./ sim_time;
 
 % sim-time vs. dt
-plot(ax(1), dt, Qfactor, '-+')
+plot(ax(1), dt, Qfactor, '-o')
 xlabel(ax(1), 'time step (ms)')
 ylabel(ax(1), 'speed factor')
 set(ax(1),'YScale','log','XScale','log')
@@ -140,12 +139,7 @@ xlabel(ax(2), 'time step (ms)')
 ylabel(ax(2), 'error (a.u.)')
 set(ax(2),'YScale','log','XScale','log')
 
-prettyFig;
-
-return
-
-
-%% Benchmark Test #1
+%% Benchmark Test #2
 % simulate a hodgkin-huxley model neuron over a series of simulation times
 
 t_end   = round(logspace(1,6,20));
@@ -154,15 +148,17 @@ Qfactor = NaN(length(t_end), 3);
 % test xolotl
 
 % set up simulation parameters for xolotl
+x.sim_dt = 0.1; % ms
 x.dt    = 0.1; % ms
 
 % perform benchmarking
 for ii = 1:length(t_end)
+  textbar(ii, length(t_end))
   % set the end time
   x.t_end = t_end(ii);
   % begin timing
   tic;
-  x.integrate(0.2);
+  V = x.integrate(0.2);
   t_sim = toc;
   % compute the speed as real-time / simulation-time
   Qfactor(ii, 1) = t_end(ii) / t_sim;
@@ -170,40 +166,44 @@ end
 
 % test DynaSim
 
-equations = { ...
-  'gNa = 1000; gKd = 300; gLeak = 1; Cm = 10', ...
-  'INa(v,m,h)=gNa.*m.^3.*h.*(v-50)',...
-  'IKd(v,n)=gKd.*n.^4.*(v+80)',...
-  'ILeak(v)=gLeak.*(v+50)',...
-  'dv/dt=(0.2/0.01-INa(v,m,h)-IKd(v,n)-ILeak(v))/Cm;',...
-  'v(0)=-65;m(0)=0;n(0)=0;h(0)=1',...
-  'dm/dt=(minf(v)-m)/taum(v)',...
-  'dh/dt=(hinf(v)-h)/tauh(v)',...
-  'dn/dt=(ninf(v)-n)/taun(v)',...
-  'minf(v)=1.0/(1.0+exp((v+25.5)/-5.29))',...
-  'hinf(v)=1.0/(1.0+exp((v+48.9)/5.18))',...
-  'ninf(v)=1.0/(1.0+exp((v+12.3)/-11.8))',...
-  'taum(v)=1.32-1.26/(1+exp((v+120.0)/-25.0))',...
-  'tauh(v)=(0.67/(1.0+exp((v+62.9)/-10.0)))*(1.5+1.0/(1.0+exp((v+34.9)/3.6)))',...
-  'taun(v)=7.2-6.4/(1.0+exp((v+28.3)/-19.2))'};
-
 for ii = 1:length(t_end)
+  textbar(ii, length(t_end))
   % begin timing
   tic;
-  data = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end(ii)], ...
-    'dt', 0.1, 'compile_flag', 1);
+  data = dsSimulate(equations, 'solver', 'rk2', 'tspan', [0 t_end(ii)], 'dt', 0.1, 'compile_flag', 0);
   t_sim = toc;
   % compute the speed as real-time / simulation-time
   Qfactor(ii, 2) = t_end(ii) / t_sim;
 end
 
-% plot the results
+% plot benchmark test #3
 
-figure;
-plot(t_end, Qfactor, '-o')
-xlabel('Simulation time (ms)')
-set(gca,'XScale','log','YScale','log','XTick',logspace(1,6,6))
-ylabel('Speed (X realtime)')
-legend({'xolotl', 'DynaSim'})
+plot(ax(3), t_end, Qfactor, '-o')
+xlabel(ax(3), 'simulation time (ms)')
+set(ax(3), 'XScale','log','YScale','log', 'XLim', [0 1.01e7], 'XTick', [1e1 1e4 1e7])
+ylabel(ax(3), 'speed factor')
+leg = legend(ax(3), {'xolotl', 'DynaSim'}, 'Location', 'EastOutside');
 
-prettyFig();
+% beautify
+prettyFig('fs', 12, 'plw', 3)
+
+% remove boxes around subplots
+for ii = 1:length(ax)
+  box(ax(ii), 'off')
+end
+
+% fix the sizing and spacing
+pos = [...
+0.0746    0.4070    0.2121    0.4937;
+0.3554    0.4070    0.2121    0.4937;
+0.6362    0.4070    0.2121    0.4937];
+
+for ii = 1:length(ax)
+  ax(ii).Position = pos(ii, :);
+end
+
+% label the subplots
+% labelFigure('capitalise', true)
+
+% break the axes
+deintersectAxes(ax(1:3))
