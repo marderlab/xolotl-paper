93ce95e95d591f9debbd8951da6e063e3b3d657a
Alec Hoyland <ahoyland@brandeis.edu>
2018-05-26 14:50:58 -0400

    minor fixes

--- neuron/benchmark2.py
+++ neuron/benchmark2.py
@@ -2,9 +2,11 @@
 # single-compartment, Hodgkin-Huxley
 # test speed with increasing simulation time
 
+# execute the main neuron file
+exec("neuron_HH.py")
+
 # set up vectors to hold outputs
-t_end           = [10, 18, 34, 62, 113, 207, 379, 695, 1274, 2336, 4281,
-                7848, 14384, 26367, 48329, 88587, 162378, 297635, 545559, 1000000]
+t_end           = np.round(np.logspace(1, 1e6, num=20))
 sim_time        = np.zeros((len(t_end),1))
 
 # perform the simulation
