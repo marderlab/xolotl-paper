2658624f640ed0da5920e367bbfb7c07850ba437
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-29 17:19:28 -0400

    minor fixes

--- neuron/benchmark1.py
+++ neuron/benchmark1.py
@@ -10,8 +10,12 @@ import time
 import neuron_HH
 
 # set up vectors to hold outputs
-t_end           = np.round(np.logspace(1, 6, num=20))
+t_end = np.array([10, 18, 34, 62, 113,  207,  379,  695, 1274, 2336, 4281, 7848,
+14384, 26367, 48329, 88587, 162378,  297635, 545559, 1000000])
+
+
 sim_time        = np.zeros((len(t_end),1))
+speed_factor    = np.zeros((len(t_end),1))
 
 # perform the simulation
 for ii in range(1,len(t_end)):
@@ -22,6 +26,7 @@ for ii in range(1,len(t_end)):
     h.run()
     toc = time.clock() - tic
     sim_time[ii] = toc;
+    speed_factor[ii] = sim_time[ii] / t_end[ii]
 
 # save the results
-np.savetxt("neuron_benchmark1.csv", sim_time, delimiter=",")
+np.savetxt("neuron_benchmark1.csv", speed_factor, delimiter=",")
