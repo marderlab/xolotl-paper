39d1d5c64c690bf9e305acbfe80d036e2cbe6000
gitlab@srinivas.gs <code@srinivas.gs>
2018-06-11 12:34:05 -0400

    minor changes to snippet

--- figures/network_snippet.m
+++ figures/network_snippet.m
@@ -3,10 +3,11 @@ x.add('AB','compartment','vol',.0628,'phi',906);
 x.add('LP','compartment','vol',.0628,'phi',906);
 x.add('PY','compartment','vol',.0628,'phi',906);
 
-% set up conductances...
+% set up conductances (not shown)
 
 % set up synapses 
 x.connect('AB','LP','Chol','gbar',30);
 x.connect('LP','AB','Glut','gbar',30);
+% and so on...
 
 [V,~,~,I_syn] = x.integrate;
