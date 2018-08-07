cce1353cf99dbebf06fdd0f80cdd6372436b0c0f
Alec Hoyland <alec.hoyland@gmail.com>
2018-08-07 15:35:14 -0400

    Completely rewrite

--- figures/benchmarks/model_stability.m
+++ figures/benchmarks/model_stability.m
@@ -3,17 +3,14 @@
 % a model with varied maximal conductances over increasing time-step
 
 % fix pseudorandom number generation
-rng(698567);
+prng = 698567;
+rng(prng);
 
 % use zoidberg to view the Prinz database
 z = zoidberg;
 z.path_to_neuron_model_db = '~/code/prinz-database/neuron-db/neuron properties';
 G = z.findNeurons('burster');
-% generate 10 models from the database
-nModels = 20;
-rand_vec = randi(length(G), nModels, 1);
-
-params = G(:, rand_vec);
+% generate 50 models from the database
 conds = {'NaV', 'CaT', 'CaS', 'ACurrent', 'KCa', 'Kd', 'HCurrent', 'Leak'};
 
 % create the xolotl model
@@ -35,14 +32,16 @@ x.dt = 1;
 % check to make sure that they are actually bursting
 disp('checking models for bursting...')
 
-if isempty(cache(GetMD5(params)))
+if isempty(cache([GetMD5(G) x.md5hash GetMD5(prng)])
   disp('running bursting tests...')
   passingModels = [];
   % set up the conductances
-  for model = 1:nModels
-    textbar(model, nModels)
+  while length(passingModels) <= 50
+    textbar(length(passingModels), 50)
+    model = randi(length(G));
+    params = G(:, model);
     for qq = 1:length(conds)
-      x.AB.(conds{qq}).gbar = params(qq, model);
+      x.AB.(conds{qq}).gbar = params;
     end
     % simulate each model
     [V, Ca] = x.integrate;
@@ -108,104 +107,77 @@ if isempty(cache(h))
       n_spikes_b(i, model) = burst_metrics(2);
       duty_cycle(i, model) = burst_metrics(9);
   	end
+  end
     % cache the results for next time
     cache(h, burst_freq, n_spikes_b, duty_cycle);
-  else
+else
     disp('pulling data from cache...')
-    [burst_freq, duty_cycle, n_spikes_b] = cache(h);
+    [Q, burst_freq, duty_cycle, n_spikes_b] = cache(h);
 end
 
 % manual override
-Q(:, 9) = NaN;
-burst_freq(:, 9) = NaN;
-duty_cycle(:, 9) = NaN;
-n_spikes_b(:, 9) = NaN;
+burst_freq(:, [2 9]) = NaN;
+duty_cycle(:, [2 9]) = NaN;
+n_spikes_b(:, [2 9]) = NaN;
 
 % generate a figure
-c = lines(size(Q, 2));
-model = 1;
-
-% set up the simulations for the insets
-for qq = 1:length(conds)
-  x.AB.(conds{qq}).gbar = params(qq, model);
-end
-
-disp('beginning high time-resolution simulation...')
-x.sim_dt = all_dt(1);
-x.dt = 1;
-x.t_end = 10e3;
-[V1, Ca1] = x.integrate;
-
-disp('beginning low-time-resolution simulation...')
-x.sim_dt = all_dt(end);
-x.dt = 1;
-[V2, Ca2] = x.integrate;
-
-t = x.dt / 1e3 * (1:length(V2)); % s
-% [V1] = rand(length(t),1);
+c = lines(size(burst_freq, 2));
 
 disp('generating figure...')
 % Place axes at (0.1,0.1) with width and height of 0.8
 fig = figure('outerposition',[100 100 1550 666],'PaperUnits','points','PaperSize',[1000 1000]);
 
 % generate axes
-for ii = 1:4
-  ax(ii) = subplot(4, 4, ii); hold on
-end
-
-counter = 0;
-for ii = 5:10
-  counter = counter + 1;
-  ax(ii) = subplot(2, 4, counter + 2); hold on
+for ii = 1:6
+  ax(ii) = subplot(3, 2, ii); hold on
 end
 
 % burst frequency
 for ii = 1:size(burst_freq, 2)
-  plot(ax(5), all_dt, burst_freq(:, ii), '-o', 'Color', c(ii, :));
+  plot(ax(1), all_dt, burst_freq(:, ii), '-o', 'Color', c(ii, :));
 end
-xlabel(ax(5), '\Deltat (ms)')
-ylabel(ax(5), 'Burst Frequency (Hz)')
-set(ax(5), 'box', 'off', 'XScale', 'log', 'YLim', [0.5, 2.0]);
+ylabel(ax(1), 'Burst Frequency (Hz)')
+set(ax(1), 'box', 'off', 'XScale', 'log', 'YLim', [0.5, 2.0]);
 
 % number of spikes per burst
 for ii = 1:size(n_spikes_b, 2)
-  plot(ax(7), all_dt, n_spikes_b(:, ii), '-o', 'Color', c(ii, :));
+  plot(ax(3), all_dt, n_spikes_b(:, ii), '-o', 'Color', c(ii, :));
 end
-xlabel(ax(7), '\Deltat (ms)')
-ylabel(ax(7), 'Spikes/Burst')
-set(ax(7), 'box', 'off', 'XScale', 'log', 'YLim', [0, max(vectorise(n_spikes_b)+5)]);
+ylabel(ax(3), 'Spikes/Burst')
+set(ax(3), 'box', 'off', 'XScale', 'log', 'YLim', [0, max(vectorise(n_spikes_b)+5)]);
 
 % duty cycle
 for ii = 1:size(duty_cycle, 2)
-  plot(ax(9), all_dt, duty_cycle(:, ii), '-o', 'Color', c(ii, :));
+  plot(ax(5), all_dt, duty_cycle(:, ii), '-o', 'Color', c(ii, :));
 end
-xlabel(ax(9), '\Deltat (ms)')
-ylabel(ax(9), 'Duty Cycle')
-set(ax(9), 'box', 'off', 'XScale', 'log', 'YLim', [0, 1.0]);
+xlabel(ax(5), '\Deltat (ms)')
+ylabel(ax(5), 'Duty Cycle')
+set(ax(5), 'box', 'off', 'XScale', 'log', 'YLim', [0, 1.0]);
 
 % burst frequency (normalized)
 for ii = 1:size(burst_freq, 2)
-  plot(ax(6), all_dt, burst_freq(:, ii), '-o', 'Color', c(ii, :));
+  plot((ax(2)), all_dt, burst_freq(:, ii) ./ burst_freq(1,ii), '-o', 'Color', c(ii, :));
 end
-set(ax(6), 'box', 'off', 'XScale', 'log', 'YLim', [0.5, 2.0]);
+set((ax(2)), 'box', 'off', 'XScale', 'log', 'YScale', 'log');
 
 % number of spikes per burst (normalized)
 for ii = 1:size(n_spikes_b, 2)
-  plot(ax(8), all_dt, n_spikes_b(:, ii), '-o', 'Color', c(ii, :));
+  plot(ax(4), all_dt, n_spikes_b(:, ii) ./ n_spikes_b(1,ii), '-o', 'Color', c(ii, :));
 end
-set(ax(8), 'box', 'off', 'XScale', 'log', 'YLim', [0, max(vectorise(n_spikes_b)+5)]);
+set(ax(4), 'box', 'off', 'XScale', 'log', 'YScale', 'log');
 
 % duty cycle (normalized)
 for ii = 1:size(duty_cycle, 2)
-  plot(ax(10), all_dt, duty_cycle(:, ii), '-o', 'Color', c(ii, :));
+  plot(ax(6), all_dt, duty_cycle(:, ii) ./ duty_cycle(1,ii), '-o', 'Color', c(ii, :));
 end
-xlabel(ax(10), '\Deltat (ms)')
-set(ax(10), 'box', 'off', 'XScale', 'log', 'YLim', [0, 1.0]);
-
-
-
-return
+xlabel(ax(6), '\Deltat (ms)')
+set(ax(6), 'box', 'off', 'XScale', 'log', 'YScale', 'log');
 
 % post-processing
 prettyFig()
+
+% remove xticks on all plots except bottom ones
+for ii = 5:8
+  set(ax(ii), 'XTick', []);
+end
 % deintersectAxes(ax(1:6))
