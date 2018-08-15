dec0e0c0805527ba85747551b7cafd68c3764224
gitlab@srinivas.gs <code@srinivas.gs>
2018-08-15 14:18:20 -0400

    switching to new V_clamp snippet

--- figures/voltage-clamp/clamp_snippet.m
+++ figures/voltage-clamp/clamp_snippet.m
@@ -2,7 +2,7 @@
 x.add('compartment','AB','A',.06)
 x.AB.add('Kd','gbar', 300);
 n = floor(x.t_end/x.sim_dt);
-V_clamp = repmat(20,n,1);
-V_clamp(1:1e3) = -60;
+V_clamp = zeros(n,1);
+V_clamp(1:1e4) = -60;
 x.V_clamp = V_clamp;
-I = x.integrate;
+I_clamp = x.integrate;
